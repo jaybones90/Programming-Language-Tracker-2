3cfa6953c78caef57dc99be4892061133a2bdead
Jason Ainsworth git config --global user.email jasoncruze@gmail.comgit config --global user.name Jason <J-Bones@Jasons-MacBook.local>
2017-03-25 14:25:13 -0700

    add working output div. add result div that works to add up values from user radio questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   $("#main-section").hide();
+  $(".answer-display").hide();
 
 
   $("#start-button").click(function(){
@@ -9,22 +10,25 @@ $(document).ready(function(){
   });
 
 
-  $(".radio").change(function(){
+
+  $("#submit-button").click(function(){
     var answer1 = parseInt($("input[name=question1]:checked").val());
     var answer2 = parseInt($("input[name=question2]:checked").val());
     var answer3 = parseInt($("input[name=question3]:checked").val());
     var answer4 = parseInt($("input[name=question4]:checked").val());
     var answer5 = parseInt($("input[name=question5]:checked").val());
-    var result = answer1 + answer2 + answer3 + answer4 + answer5
-
-    $("#submit-button").click(function(){
-
-      alert(result);
-      //
-    });
-  });
+    var result = answer1 + answer2 + answer3 + answer4 + answer5;
+    $("#main-section, .answer-display").fadeToggle(500);
     // $("#output").text(result);
-    // $(".display").fadeToggle();
+
+    if (result < 3) {
+      $("#output").html("You Should Take The Ruby/Rails Track!")
+    } else {
+      $("output").text("you fucked up")
+    }
+
+  });
+
 
 
 });
