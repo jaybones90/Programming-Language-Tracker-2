e6309ddd8afc812ebe9ad7765cdf0559ceb350d2
Jason Ainsworth git config --global user.email jasoncruze@gmail.comgit config --global user.name Jason <J-Bones@Jasons-MacBook.local>
2017-03-24 22:28:20 -0700

    add jquery for answer variables. it will return accurate value for answer1 but not after hitting submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
 
-$("#main-section").hide();
-
-$("#start-button").click(function(){
-  $("#main-section").fadeToggle(900);
-})
+  $("#main-section").hide();
 
 
+  $("#start-button").click(function(){
+    $("#main-section").fadeToggle(1500);
+    $("#start-section").fadeToggle();
+  });
+
+  var answer1 = parseInt($("input[name=question1]:checked").val());
+  var answer2 = parseInt($("input[name=question2]:checked").val());
+  var answer3 = parseInt($("input[name=question3]:checked").val());
+  var answer4 = parseInt($("input[name=question4]:checked").val());
+  var answer5 = parseInt($("input[name=question5]:checked").val());
+  var result = answer1 + answer2 + answer3 + answer4 + answer5
+
+    // $("#output").text(result);
+    // $(".display").fadeToggle();
 
 
+  $("#submit-button").click(function(){
+    alert(result);
 
-
-
-
-
+  });
 });
