f6fa37f3da6961e2d8bff3ef6a4e3fd7fc9cae63
Jason Ainsworth git config --global user.email jasoncruze@gmail.comgit config --global user.name Jason <J-Bones@Jasons-MacBook.local>
2017-03-25 13:01:00 -0700

    add alert to make sure variables were returning correct values. now the result variable alerts 5 different answers

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,23 @@ $(document).ready(function(){
     $("#start-section").fadeToggle();
   });
 
-  var answer1 = parseInt($("input[name=question1]:checked").val());
-  var answer2 = parseInt($("input[name=question2]:checked").val());
-  var answer3 = parseInt($("input[name=question3]:checked").val());
-  var answer4 = parseInt($("input[name=question4]:checked").val());
-  var answer5 = parseInt($("input[name=question5]:checked").val());
-  var result = answer1 + answer2 + answer3 + answer4 + answer5
 
+  $(".radio").change(function(){
+    var answer1 = parseInt($("input[name=question1]:checked").val());
+    var answer2 = parseInt($("input[name=question2]:checked").val());
+    var answer3 = parseInt($("input[name=question3]:checked").val());
+    var answer4 = parseInt($("input[name=question4]:checked").val());
+    var answer5 = parseInt($("input[name=question5]:checked").val());
+    var result = answer1 + answer2 + answer3 + answer4 + answer5
+
+    $("#submit-button").click(function(){
+
+      alert(result);
+      //
+    });
+  });
     // $("#output").text(result);
     // $(".display").fadeToggle();
 
 
-  $("#submit-button").click(function(){
-    alert(result);
-
-  });
 });
