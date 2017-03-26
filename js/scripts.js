c56aa345b2cccc310724e89de321162dd70049de
Jason Ainsworth git config --global user.email jasoncruze@gmail.comgit config --global user.name Jason <J-Bones@Jasons-MacBook.local>
2017-03-25 18:53:23 -0700

    add if else statements to display different suggestions to output variable depending on user answer to radio question. Everything working great. Add styling to survey and index page.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
 
   $("#main-section").hide();
   $(".answer-display").hide();
+  $("#submit-button").hide();
 
 
   $("#start-button").click(function(){
     $("#main-section").fadeToggle(1500);
     $("#start-section").fadeToggle();
+    $("#submit-button").fadeToggle();
   });
 
 
@@ -19,12 +21,20 @@ $(document).ready(function(){
     var answer5 = parseInt($("input[name=question5]:checked").val());
     var result = answer1 + answer2 + answer3 + answer4 + answer5;
     $("#main-section, .answer-display").fadeToggle(500);
-    // $("#output").text(result);
+    $("#submit-button").hide();
 
-    if (result < 3) {
-      $("#output").html("You Should Take The Ruby/Rails Track!")
-    } else {
-      $("output").text("you fucked up")
+
+
+    if (result <= 6) {
+      $("#output").text("You Should Take The Ruby/Rails Track!")
+    } else if (result <= 8 ) {
+      $("#output").text("You Should Take The Java/Android Track!")
+    } else if (result <= 10) {
+      $("#output").text("You Should Take The PHP/Drupal Track!")
+    } else if (result <= 12) {
+      $("#output").text("You Should Take The CSS/Design Track!")
+    } else if (result <= 15) {
+      $("#output").text("You Should Take The C#/.NET Track!")
     }
 
   });
